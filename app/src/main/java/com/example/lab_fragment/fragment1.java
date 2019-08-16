d7216826b3d6a31f48f2a53776d781521ee5cb09
Vishal Kumar Singh <01vksingh@gmail.com>
2019-08-16 12:29:03 +0530

    fragment added some part

---
+++ app/src/main/java/com/example/lab_fragment/fragment1.java
@@ -0,0 +1,24 @@
+package com.example.lab_fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class fragment1 extends Fragment {
+
+    ImageView i1;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.first,container,false);
+        return view;
+    }
+}
